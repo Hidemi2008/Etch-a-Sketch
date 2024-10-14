fc8200cd1aab1e3e478a583d1d31bbebaa9d5aaf
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-10-14 12:28:45 -0400

    Create the respository

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let container = document.querySelector("#container")
+let div = document.createElement('div')
+div.style.backgroundColor = "red"
+div.style.height = "256px"
+div.style.width = "256px"
+
+container.appendChild(div)
