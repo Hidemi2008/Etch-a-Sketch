235e6ec0350f1654c338c8d6ea89e289511eba5e
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-10-16 18:07:07 -0400

    Complete the Sketch

--- javascript.js
+++ javascript.js
@@ -1,52 +1,73 @@
-let black = "black"
+let color = "black"
+let click = false
 
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16)
 
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click
+            let draw = document.querySelector("#draw")
+            if(click){
+                draw.innerHTML = "Now you can draw"
+            }else{
+                draw.innerHTML = "You're now allowed to draw"
+            }
+        }
+    })
+    
     let btn_popup = document.querySelector("#popup")
-    btn_popup.addEventListener('click', function(){
+    btn_popup.addEventListener("click", function(){
         let size = getSize()
         createBoard(size)
     })
 })
 
 function createBoard(size){
-    let container = document.querySelector("#border")
+    let board = document.querySelector(".board")
 
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
     let numDivs = size * size
 
-    for(let i = 0; i < numDivs; i++){
-        let div = document.createElement("div")
-        div.addEventListener('mouseover', colorDivs)
-        container.insertAdjacentElement
-        ("beforeend", div)
+    for(let i = 0;i < numDivs; i++ ){
+        let div = document.createElement('div')
+        div.addEventListener("mouseover",colorDiv)
+        board.insertAdjacentElement('beforeend', div)
     }
 }
 
 function getSize(){
-    let choose = window.prompt("what will be the size of the board?")
+    let input = prompt("What will be the size of the board")
     let message = document.querySelector("#message")
-    if(choose == ""){
+    if(input == ""){
         message.innerHTML = "Please provide a number"
     }else{
         if(input < 0 || input > 100){
             message.innerHTML = "Provide a number between 1 and 100"
         }else{
-            message.innerHTML = "Now you can play!"
-            return choose
+            message.innerHTML = "Now you can play"
+            return input
         }
     }
 }
 
-function colorDivs(){
-    if(colorChoise == 'random'){
-        this.style.backgroundColor = ""
+function colorDiv(){
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }else{
+            this.style.backgroundColor = "black"
+        }
     }
 }
 
 function setColor(colorChoise){
-    let color = colorChoise
+     color = colorChoise
+}
+
+function resetBoard(){
+    let divs = document.querySelector("div")
+    divs.forEach((div) => div.style.backgroundColor  = "white")
 }
